52176f17e4eff056e2363a86837ce4c18d99e603
Aayush Kunwar <141144348+aayush-hash@users.noreply.github.com>
2025-09-27 14:45:03 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,4 @@
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import { app, server } from "./lib/socket.js";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,19 +9,6 @@ dotenv.config();
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
-  app.use(express.static(frontendPath));
-
-  app.get(/^(?!\/api).*$/, (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
 
 // Connect DB and start server
 const PORT = process.env.PORT || 5000;
